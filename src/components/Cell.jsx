c3103bbc7a074d617227a9fcf9c7e29d860c0f48
Mario Madera <mariodml90@gmail.com>
2021-12-10 16:03:26 -0500

    add files

---
+++ src/components/Cell.jsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
+
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+);
+
+export default memo(Cell);
